eaaa32cda3c8e93c083a0711570bbf93faab42ac
matheusapostulo <matheusapostulo10@gmail.com>
2024-09-10 18:17:19 -0300

    :recycle: refactor: First communication with the fastapi python service

--- backend/backend-express/src/application/strategy/LungCancerModelPredictionStrategy.ts
+++ backend/backend-express/src/application/strategy/LungCancerModelPredictionStrategy.ts
@@ -4,27 +4,29 @@ export default class LungCancerModelPredictionStrategy implements ModelPredictio
     constructor(readonly modelAPIUrl: string){}
     
     async predict(): Promise<any> {
-        // const data = {
-        //     gender: 2,
-        //     age: 69,
-        //     smoker: 1,
-        //     yellow_fingers: 2,
-        //     anxiety: 2,
-        //     peer_pressure: 1,
-        //     chronic_disease: 1,
-        //     fatigue: 2,
-        //     allergy: 1,
-        //     wheezing: 2,
-        //     alcohol_consume: 2,
-        //     coughing: 2,
-        //     shortness_of_breath: 2,
-        //     swallowing_difficulty: 2,
-        //     chest_pain: 2
-        // };
-        // await axios.post("http://localhost:4000/predict/lung-cancer", data)
-        // .then((res) => {
-        //     return res.data;
-        // });
-        return 'Result';
+        const data = {
+            gender: 2,
+            age: 69,
+            smoker: 1,
+            yellow_fingers: 2,
+            anxiety: 2,
+            peer_pressure: 1,
+            chronic_disease: 1,
+            fatigue: 2,
+            allergy: 1,
+            wheezing: 2,
+            alcohol_consume: 2,
+            coughing: 2,
+            shortness_of_breath: 2,
+            swallowing_difficulty: 2,
+            chest_pain: 2
+        };
+        try {
+            const res = await axios.post("http://host.docker.internal:4000/predict/lung-cancer", data)
+            return res.data.prediction.toString();
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
     }
 }
