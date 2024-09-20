af587b755051deb19053863c2137aafdb920e4b8
matheusapostulo <matheusapostulo10@gmail.com>
2024-09-20 14:51:26 -0300

    :sparkles: feat: New folder structure on the nextjs front-end

--- frontend-next-js/src/app/layout.tsx
+++ frontend-next-js/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "../components/Header";
 
 const lato = Lato({ weight: ['100', '300', '400', '700', '900'], subsets: ["latin"] });
 
--- frontend-next-js/src/app/page.tsx
+++ frontend-next-js/src/app/page.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 export default function Home() { 
   return (
     <div className="flex flex-col justify-center">
       <p className="text-black-200 text-lg">conteúdo</p>
+      {/* <button onClick={fetchData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buscar</button> */}
     </div>
   );
 }
