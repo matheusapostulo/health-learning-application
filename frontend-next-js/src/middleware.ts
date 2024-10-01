12c3d9be44f55dc3d7bf419f4612bd1d278c39c1
matheusapostulo <matheusapostulo10@gmail.com>
2024-10-01 18:04:18 -0300

    :truck: chore: Moving the middleware correctly into src

--- frontend-next-js/src/middleware.ts
+++ frontend-next-js/src/middleware.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import authConfig from "./auth.config";
 import { NextResponse } from "next/server";
+import authConfig from "../auth.config";
 
 const { auth } = NextAuth(authConfig);
 
